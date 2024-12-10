4b5a12dd4d210c0c082e4d78d77cc043a0e3a21c
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2024-12-10 11:16:39 +0100

    stylet splash modal

--- _frontend/src/ts/components/widgets/splash-modal.tsx
+++ _frontend/src/ts/components/widgets/splash-modal.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import Table from 'react-table'
+import { RecentSales } from '../views/recent-sales';
 
-export const SplashModal = () => {
+export const SplashModal = (resentSale: RecentSales) => {
 	return (
-		''
+		<div className=' fixed top-0 left-0 w-full  h-16 '>
+			<div className='bg-[#3639A4] text-[#FAFAFB] text-center py-3 w-4/12 mx-auto rounded-md' >
+				<p className='text-lg'>{resentSale.name}</p>
+				<p className='text-lg'>solgt {resentSale.product} for {resentSale.price} </p>
+			</div>
+		</div>
 	)
 }
