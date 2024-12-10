581d7d568f65ec0290d9326efbdfecf752e045a5
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2024-12-10 15:28:42 +0100

    scoreboard change after time

--- _frontend/src/ts/components/dashboard.tsx
+++ _frontend/src/ts/components/dashboard.tsx
@@ -1,28 +1,69 @@
-import React from 'react';
-import { RecentSalesView } from './views/recent-sales';
-import { TopSalesView } from './views/top-sales';
+import React, { useState, useContext, useEffect } from 'react';
+import { RecentSalesView, RecentSales } from './views/recent-sales';
+import { TopSalesView, TopSaler } from './views/top-sales';
 import { SplashModal } from './widgets/splash-modal';
 import { Header } from './views/header';
 import { SalesConnnectorContext } from '../context/sales-connector';
 
+interface TopSalerScoreBoard{
+	saler: Array<TopSaler>
+}
+
+interface RecentSalerScoreBoard{
+	sale: Array<RecentSales>
+}
+
 export const DashBoardView = () => {
-	const { hub, store } = React.useContext(SalesConnnectorContext)
+	const { hub, store } = useContext(SalesConnnectorContext)
 
-	const [mode, setMode] = React.useState<'top' | 'recent'>('top');
-	const [splash, setSplash] = React.useState<boolean>(true);
+	const [mode, setMode] = useState<'top' | 'recent'>('top');
+	const [splash, setSplash] = useState<boolean>(true);
+	const [salesDepartmentTopSales, setSalesDepartmentTopSales] = useState<TopSalerScoreBoard>({saler: [{name: '', totalPrice: 0}]})
+	const [salesDepartmentRecentSales, setSalesDepartmentRecentSales] = useState<RecentSalerScoreBoard>({sale: [{name: '', price: 0, product: ''}]})
+	let recentSalesArray:  Array<RecentSales> = []
 
-	React.useEffect(() => {
+	useEffect(() => {
 		// initialize callback
 		const cb = async (e) => {
 			let user = await store.getUser(e.userId)
 			let product = await store.getProduct(e.productId)
 
+			recentSalesArray.push({name: user.name, price: product.unitPrice, product: product.name})
+			// setSalesDepartmentRecentSales({sale: recentSalesArray})
+
+			// setSalesDepartmentRecentSales((prevState) => {
+			// 	return {sale: recentSalesArray}
+			// })
+
+			// setSalesDepartmentRecentSales((prevState) => {
+			// 	return {...prevState, sale: recentSalesArray}
+			// })
+
+			// console.log(recentSalesArray)
+			// console.log(salesDepartmentRecentSales)
 			console.log('User', user.name, 'sold', product.name, 'with subscription length', e.duration)
 		}
 
+		
 		hub.registerSalesEventListener(cb)
 		return () => hub.unregisterSalesEventListener(cb)
 	}, []);
+
+	useEffect(() => {
+		console.log(mode)
+		if(mode == 'top'){
+			const timeOut = setTimeout(() => {
+				console.log('set to recent')
+				setMode('recent')
+			}, 90000)
+		} else {
+			const timeOut = setTimeout(() => {
+				console.log('set to top')
+				setMode('top')
+			}, 30000)
+		}
+	}, [mode])
+
 
 	return (
 		<>
@@ -34,7 +75,7 @@ export const DashBoardView = () => {
 				}
 
 				{splash &&
-					<SplashModal />
+					<SplashModal name={'Jørn Nørregaard Smed'} price={79.95} product={'Curious tails and other short stories'} />
 				}
 			</div>
 		</>
