e91a380a3bc8c8324663ac69b992e62c009d4f22
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2024-12-10 11:17:08 +0100

    startet on stuling recent-sales.tsx

--- _frontend/src/ts/components/views/recent-sales.tsx
+++ _frontend/src/ts/components/views/recent-sales.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import Table from '../widgets/table';
+import { Card } from '../widgets/card';
+
+export interface RecentSales{
+	name: string;
+	price: number;
+	product: string;
+}
 
 export const RecentSalesView = () => {
 	return (
-		''
+		<>
+			<h2>Recent Sales</h2>
+			<Card>
+				<Card.InsetBody>
+					<Table>
+						<Table.Headers>
+							<Table.Header>User</Table.Header>
+							<Table.Header>Magazine</Table.Header>
+							<Table.Header>Price</Table.Header>
+						</Table.Headers>
+						<Table.Body>
+							<Table.Row>
+								<Table.Cell>Scratchy</Table.Cell>
+								<Table.Cell>Magazine</Table.Cell>
+								<Table.Cell>1337</Table.Cell>
+							</Table.Row>
+							<Table.Row>
+								<Table.Cell>Felix</Table.Cell>
+								<Table.Cell>Magazine</Table.Cell>
+								<Table.Cell>1337</Table.Cell>
+							</Table.Row>
+							<Table.Row>
+								<Table.Cell>Tomcat</Table.Cell>
+								<Table.Cell>Magazine</Table.Cell>
+								<Table.Cell>1337</Table.Cell>
+							</Table.Row>
+						</Table.Body>
+					</Table>
+				</Card.InsetBody>
+			</Card>
+		</>
 	)
 }
