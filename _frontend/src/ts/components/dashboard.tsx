4c174d0891b5ea2a5f78e6ddf97fa0f531122b20
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2024-12-11 16:11:46 +0100

    made the scorboard topsaler array

--- _frontend/src/ts/components/dashboard.tsx
+++ _frontend/src/ts/components/dashboard.tsx
@@ -1,13 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { RecentSalesView, RecentSales } from './views/recent-sales';
-import { TopSalesView, TopSaler } from './views/top-sales';
+import { TopSalesView, TopSaler, TopSalerScoreBoard } from './views/top-sales';
 import { SplashModal } from './widgets/splash-modal';
 import { Header } from './views/header';
 import { SalesConnnectorContext } from '../context/sales-connector';
-
-interface TopSalerScoreBoard{
-	saler: Array<TopSaler>
-}
 
 interface RecentSalerScoreBoard{
 	sale: Array<RecentSales>
@@ -18,29 +14,46 @@ export const DashBoardView = () => {
 
 	const [mode, setMode] = useState<'top' | 'recent'>('top');
 	const [splash, setSplash] = useState<boolean>(true);
-	const [salesDepartmentTopSales, setSalesDepartmentTopSales] = useState<TopSalerScoreBoard>({saler: [{name: '', totalPrice: 0}]})
+	const [salesDepartmentTopSales, setSalesDepartmentTopSales] = useState<TopSalerScoreBoard>({saler: [{id: 0, name: '', totalPrice: 0}]})
 	const [salesDepartmentRecentSales, setSalesDepartmentRecentSales] = useState<RecentSalerScoreBoard>({sale: [{name: '', price: 0, product: ''}]})
-	let recentSalesArray:  Array<RecentSales> = []
+	const topSalerArray: Array<TopSaler> = []
+	const recentSalesArray: Array<RecentSales> = []
 
 	useEffect(() => {
 		// initialize callback
 		const cb = async (e) => {
 			let user = await store.getUser(e.userId)
 			let product = await store.getProduct(e.productId)
-
+			
 			recentSalesArray.push({name: user.name, price: product.unitPrice, product: product.name})
-			// setSalesDepartmentRecentSales({sale: recentSalesArray})
+			setSalesDepartmentRecentSales((prevState) => {
+				return {...prevState, sale: recentSalesArray}
+			})
 
-			// setSalesDepartmentRecentSales((prevState) => {
-			// 	return {sale: recentSalesArray}
-			// })
+			if(topSalerArray.length == 0){ // only add if there is not any element in the array
+				topSalerArray.push({id: user.id, name: user.name, totalPrice: product.unitPrice})
+			} else {
 
-			// setSalesDepartmentRecentSales((prevState) => {
-			// 	return {...prevState, sale: recentSalesArray}
-			// })
+				let sameName = false // To check the topSalerArray for already excited elements in the array
+				for (let i = 0; i < topSalerArray.length; i++) {
+					if(topSalerArray[i].name == user.name){
+						topSalerArray[i].totalPrice=+ product.unitPrice // Add the unitPrice to the already excited person with the same name
+						sameName = true
+					}
+				}
 
-			// console.log(recentSalesArray)
-			// console.log(salesDepartmentRecentSales)
+
+				if(sameName == false){ // Only add to the array if sameName is false, to know there is not a person with the same name in the array
+					topSalerArray.push({id: user.id, name: user.name, totalPrice: product.unitPrice})
+				}
+			}
+			
+			topSalerArray.sort(function(a,b){return b.totalPrice - a.totalPrice}) // Sort the element in topSalerArray after the highest number
+			setSalesDepartmentTopSales((prevState) => {
+				return {...prevState, saler: topSalerArray}
+			})
+							
+
 			console.log('User', user.name, 'sold', product.name, 'with subscription length', e.duration)
 		}
 
@@ -49,8 +62,9 @@ export const DashBoardView = () => {
 		return () => hub.unregisterSalesEventListener(cb)
 	}, []);
 
+
+
 	useEffect(() => {
-		console.log(mode)
 		if(mode == 'top'){
 			const timeOut = setTimeout(() => {
 				console.log('set to recent')
@@ -64,14 +78,13 @@ export const DashBoardView = () => {
 		}
 	}, [mode])
 
-
 	return (
 		<>
 			<div className="flex-auto p-5">
 				<Header />
 				{mode === 'recent' ?
 					<RecentSalesView />
-					: <TopSalesView />
+					: <TopSalesView saler={salesDepartmentTopSales.saler} />
 				}
 
 				{splash &&
