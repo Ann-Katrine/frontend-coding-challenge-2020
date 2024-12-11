07c3a2bc531f6452e88dc337ad3f3efb2f403bef
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2024-12-11 16:13:11 +0100

    change hardcode data to dynamsik data

--- _frontend/src/ts/components/views/top-sales.tsx
+++ _frontend/src/ts/components/views/top-sales.tsx
@@ -2,31 +2,53 @@ import React from 'react';
 import Table from '../widgets/table';
 import { Card } from '../widgets/card';
 
-export const TopSalesView = () => {
+export interface TopSaler{
+	id: number;
+	name: string;
+	totalPrice: number;
+}
+
+export interface TopSalerScoreBoard{
+	saler: Array<TopSaler>
+}
+
+export const TopSalesView = (scoreboardInfo: TopSalerScoreBoard) => {
 	return (
-		<Card>
-			<Card.InsetBody>
-				<Table>
-					<Table.Headers>
-						<Table.Header>User</Table.Header>
-						<Table.Header>Value</Table.Header>
-					</Table.Headers>
-					<Table.Body>
-						<Table.Row>
-							<Table.Cell>Scratchy</Table.Cell>
-							<Table.Cell>1337</Table.Cell>
-						</Table.Row>
-						<Table.Row>
-							<Table.Cell>Felix</Table.Cell>
-							<Table.Cell>1337</Table.Cell>
-						</Table.Row>
-						<Table.Row>
-							<Table.Cell>Tomcat</Table.Cell>
-							<Table.Cell>1337</Table.Cell>
-						</Table.Row>
-					</Table.Body>
-				</Table>
-			</Card.InsetBody>
-		</Card>
+		<>
+			<h2>Top sellers</h2>
+			<Card>
+				<Card.InsetBody>
+					<Table>
+						<Table.Headers>
+							<Table.Header>User</Table.Header>
+							<Table.Header>Value</Table.Header>
+						</Table.Headers>
+						<Table.Body>
+							{ scoreboardInfo.saler.length != 0 ? 
+								<>
+									{/* Using the slice() to only show 10 element of the array   */}
+									{scoreboardInfo.saler.slice(0, 10).map((item) =>(
+										<>
+											<Table.Row key={item.name + item.id}>
+												<Table.Cell> {item.name} </Table.Cell>
+												<Table.Cell>{item.totalPrice}</Table.Cell>
+											</Table.Row>
+										</>
+									))}
+								
+								</> 
+								: 
+								<>
+									<Table.Row>
+										<p>There has not been found any sales in the system...</p>
+									</Table.Row>
+								</>
+							}
+						</Table.Body>
+					</Table>
+				</Card.InsetBody>
+			</Card>
+			
+		</>
 	)
 }
