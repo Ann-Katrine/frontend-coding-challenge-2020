44e774963205e14cf79280462b98778085d2158c
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2024-12-12 20:36:10 +0100

    splash notification work now

--- _frontend/src/ts/components/dashboard.tsx
+++ _frontend/src/ts/components/dashboard.tsx
@@ -5,16 +5,30 @@ import { SplashModal } from './widgets/splash-modal';
 import { Header } from './views/header';
 import { SalesConnnectorContext } from '../context/sales-connector';
 
+interface User{
+	id: number;
+	name: string;
+	type: string;
+}
+
+interface Product{
+	id: number;
+	name: string;
+	type: string;
+	unitPrice: number;
+}
+
 export const DashBoardView = () => {
 	const { hub, store } = useContext(SalesConnnectorContext)
 
 	const [mode, setMode] = useState<'top' | 'recent'>('top');
-	const [splash, setSplash] = useState<boolean>(true);
-	const [salesDepartmentTopSales, setSalesDepartmentTopSales] = useState<TopSalerScoreBoard>({saler: [{id: 0, name: '', totalPrice: 0}]})
-	const [salesDepartmentRecentSales, setSalesDepartmentRecentSales] = useState<RecentSalerScoreBoard>({sale: [{id: 0, name: '', price: 0, product: ''}]})
+	const [splash, setSplash] = useState<boolean>(false);
+	const [salesDepartmentTopSales, setSalesDepartmentTopSales] = useState<TopSalerScoreBoard>({saler: []})
+	const [salesDepartmentRecentSales, setSalesDepartmentRecentSales] = useState<RecentSalerScoreBoard>({sale: []})
+	const [saleNotification, setSaleNotification] = useState<RecentSalerScoreBoard>({sale: []})
+	const [isProcessing, setIsProcessing] = useState<boolean>(false)
 	const topSalerArray: Array<TopSaler> = []
 	const recentSalesArray: Array<RecentSales> = []
-	const recentSalesNotificationArray: Array<RecentSales> = []
 
 	useEffect(() => {
 		// initialize callback
@@ -22,14 +36,10 @@ export const DashBoardView = () => {
 			let user = await store.getUser(e.userId)
 			let product = await store.getProduct(e.productId)
 			
-			recentSalesNotificationArray.push({id: user.id, name: user.name, price: product.unitPrice, product: product.name})
-			
-
+			addToSaleNotifiction(user, product)
 			addToSalesDeparmentTopSales(user, product)
-			addSaleDepartmentRecentSales(user, product)
+			addToSaleDepartmentRecentSales(user, product)
 			
-
-
 			console.log('User', user.name, 'sold', product.name, 'with subscription length', e.duration)
 		}
 
@@ -52,8 +62,49 @@ export const DashBoardView = () => {
 		}
 	}, [mode])
 
-	// Add element to saleDeparmentRecentSale
-	function addSaleDepartmentRecentSales(user, product){
+	const processNotification = async (item: RecentSalerScoreBoard) => {
+		for (let i = 0; i < item.sale.length; i++) {
+			setSplash(true)
+
+
+			await new Promise<void>((resolve) => {
+				setTimeout(() => {
+					setSplash(false)
+					resolve()
+				}, 5000)
+			})
+
+			setSaleNotification((prevState) => {
+				const update = [...prevState.sale] // Copy the existing 'sale' data
+				update.shift() // remove the first element in the array, so it not be shown again
+				return {sale: update} // overwrite  the existing data with the new from update
+			})
+		}
+
+		setIsProcessing(false)
+	}
+
+	useEffect(() => {
+		if(saleNotification.sale.length > 0 && isProcessing == false){
+			setIsProcessing(true)
+			processNotification(saleNotification)
+		}
+	}, [saleNotification, isProcessing])
+
+	// Add element to saleNotification
+	function addToSaleNotifiction(user: User, product: Product){
+		let recentSalesNotificationArray: Array<RecentSales> = []
+
+		recentSalesNotificationArray.push({id: user.id, name: user.name, price: product.unitPrice, product: product.name})
+		setSaleNotification((prevState) => {
+			return {...prevState, sale: [...prevState.sale, ...recentSalesNotificationArray]} 
+			// [...prevState.sale, ...recentSalesNotificationArray] works so you will get the value that is allready in the saleNotification 
+			// and the new element from recentSalesNotificationArray, will be merge togeter
+		})
+	}
+
+	// Add element to saleDepartmentRecentSales
+	function addToSaleDepartmentRecentSales(user: User, product: Product){
 		if(recentSalesArray.length == 0){ // only add if there is not any element in the array
 			recentSalesArray.push({id: user.id, name: user.name, price: product.unitPrice, product: product.name})
 		} else {
@@ -67,11 +118,13 @@ export const DashBoardView = () => {
 
 		setSalesDepartmentRecentSales((prevState) => {
 			return {...prevState, sale: recentSalesArray}
+			// {...prevState, sale: recentSalesArray} works så that sale: recentSalesArray will overwrite what 
+			// value there would have been in saleDepartmentRecentSales
 		})
 	}
 
 	// Add element to salesDeparmentTopSales
-	function addToSalesDeparmentTopSales(user, product){
+	function addToSalesDeparmentTopSales(user: User, product: Product){
 		if(topSalerArray.length == 0){ // only add if there is not any element in the array
 			topSalerArray.push({id: user.id, name: user.name, totalPrice: product.unitPrice})
 		} else {
@@ -88,7 +141,7 @@ export const DashBoardView = () => {
 				topSalerArray.push({id: user.id, name: user.name, totalPrice: product.unitPrice})
 			}
 		}
-		
+
 		topSalerArray.sort(function(a,b){return b.totalPrice - a.totalPrice}) // Sort the element in topSalerArray after the highest number
 		setSalesDepartmentTopSales((prevState) => {
 			return {...prevState, saler: topSalerArray}
@@ -104,8 +157,8 @@ export const DashBoardView = () => {
 					: <TopSalesView saler={salesDepartmentTopSales.saler} />
 				}
 
-				{splash &&
-					<SplashModal id={0} name={'Jørn Nørregaard Smed'} price={79.95} product={'Curious tails and other short stories'} />
+				{saleNotification.sale.length != 0 && splash &&
+					<SplashModal id={saleNotification.sale[0]?.id} name={saleNotification.sale[0]?.name} price={saleNotification.sale[0]?.price} product={saleNotification.sale[0]?.product} />
 				}
 			</div>
 		</>
